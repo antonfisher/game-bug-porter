4303c6f74230e3cb6e63cc9ee4aee6f7376b2d41
Anton Fisher <a.fschr@gmail.com>
2016-07-06 23:34:24 -0700

    Add webpack, package files

---
+++ src/utils.js
@@ -0,0 +1,3 @@
+/**
+ * Created by af on 7/6/16.
+ */
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+'use strict'
+
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'stage-0'],
+        plugins: [
+          ['transform-decorators-legacy']
+        ]
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: './index.html',
+      inject: 'body',
+      title: 'Application'
+    }),
+    new CopyWebpackPlugin([
+      {from: 'phaser/build/phaser.min.js'},
+      {from: 'assets/*'}
+    ])
+  ],
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+    stats: true
+  }
+}
