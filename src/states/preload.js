2f840604bcb3ecf58ca458c7cffdb569e97c98f1
Anton Fisher <a.fschr@gmail.com>
2016-07-10 23:03:20 -0700

    Update preload

--- src/states/preload.js
+++ src/states/preload.js
@@ -1,9 +1,8 @@
 'use strict'
 
-export class Group {
-
-  constructor (game) {
-    this._game = game;
-  }
-
+export function preload (game) {
+  game.load.spritesheet('bag', '/assets/bag.png', 140, 180)
+  game.load.spritesheet('bug', '/assets/bug.png', 320, 210)
+  game.load.image('bg-wood', '/assets/bg-wood.png', 320, 320)
+  game.load.image('bg-floor', '/assets/bg-floor.png', 320, 120)
 }
