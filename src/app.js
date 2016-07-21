55f612b5253815350d49d325d34458176d71b6f7
Anton Fisher <a.fschr@gmail.com>
2016-07-21 00:11:36 -0700

    Auto scale game

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import {create} from './states/create.js'
 import {update} from './states/update.js'
 
 const el = window.document.getElementById('game')
-const game = new Phaser.Game(900, 1600, Phaser.AUTO, el, {preload, create, update}, true, false)
+const height = 1600;
+const width = (el.clientWidth * height / el.clientHeight)
+const game = new Phaser.Game(width, height, Phaser.AUTO, el, {preload, create, update}, true, false)
 
 Global.set('game', game)
